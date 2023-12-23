11720c38cbd98942eaf0fc558f57c9065ede9eb0
mohd-ashif <mohdaszif077@gmail.com>
2023-12-23 11:06:40 +0530

    add method- 2

--- starItUp.js
+++ starItUp.js
@@ -1,6 +1,7 @@
 function starItUp(a) {
     let space = "";
     let n = a.length;
+   
 
     for (let i = 0; i < n; i++) {
         space += a[i];
@@ -13,3 +14,23 @@ function starItUp(a) {
 }
 
 starItUp('abc');
+
+
+// method -2
+var a = "hello";
+var n = a.length;
+var c = '';
+
+function star() {
+    return "*";
+}
+
+for (var i = 0; i < n; i++) {
+    if (i < n - 1) {
+        c += a[i] + star();
+    } else {
+        c += a[i];
+    }
+}
+
+console.log(c);
